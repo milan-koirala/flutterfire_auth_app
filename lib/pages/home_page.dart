edffe1885e7684b4a00a0f04c5aa84564f4b9539
Milan Koirala <koiralamilon67@gmail.com>
2025-06-22 23:50:33 +0545

    Add logout button to HomePage using FirebaseAuth

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  // sign user out method
+  void signUserOut() async {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: signUserOut,
+            icon: Icon(Icons.logout
+            )
+          ),
+        ],
+      ),
       body: Center(child: Text("LOGGED IN!"),),
     );
   }
