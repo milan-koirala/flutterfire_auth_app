a80865047891bbf0246181166f99e257a07ab659
Milan Koirala <koiralamilon67@gmail.com>
2025-06-23 22:56:20 +0545

    Display logged-in user's email on HomePage and style AppBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final user = FirebaseAuth.instance.currentUser!;
 
   // sign user out method
   void signUserOut() async {
@@ -13,15 +15,20 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.black,
         actions: [
           IconButton(
             onPressed: signUserOut,
-            icon: Icon(Icons.logout
-            )
+            icon: Icon(Icons.logout, color: Colors.white),
           ),
         ],
       ),
-      body: Center(child: Text("LOGGED IN!"),),
+      body: Center(
+        child: Text(
+          "LOGGED IN AS: " + user.email!,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
     );
   }
 }
