78c66097dbe0227f595fb8f254ff9b6d69fc79ec
Milan Koirala <koiralamilon67@gmail.com>
2025-06-23 22:49:35 +0545

    Add LoginOrRegisterPage to toggle between login and register screens

---
+++ lib/pages/login_or_register_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutterfire_auth/pages/login_page.dart';
+import 'package:flutterfire_auth/pages/register_page.dart';
+
+class LoginOrRegisterPage extends StatefulWidget {
+  const LoginOrRegisterPage({super.key});
+
+  @override
+  State<LoginOrRegisterPage> createState() => _LoginOrRegisterPageState();
+}
+
+class _LoginOrRegisterPageState extends State<LoginOrRegisterPage> {
+  // initially show login page
+  bool showLoginPage = true;
+
+  // toogle between login and register page
+  void togglePages() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginPage(
+        onTap: togglePages,
+      );
+    } else {
+      return RegisterPage(
+        onTap: togglePages,
+      );
+    }
+  }
+}
