8db0b169dc77b8baaf7ffb3486c8e37d18a3f2a0
Milan Koirala <koiralamilon67@gmail.com>
2025-06-24 23:03:58 +0545

    Implement functional ForgotPasswordPage with email input and Firebase password reset

--- lib/pages/forgot_pass_page.dart
+++ lib/pages/forgot_pass_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ForgotPasswordPage extends StatefulWidget {
@@ -8,10 +9,70 @@ class ForgotPasswordPage extends StatefulWidget {
 }
 
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final _emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  Future passwordReset() async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(
+        email: _emailController.text.trim(),
+      );
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Enter your email and we will send you a password reset link"),
+      appBar: AppBar(backgroundColor: Colors.deepPurple[200], elevation: 0),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Text(
+              "Enter your email and we will send you a password reset link",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+
+          SizedBox(height: 10),
+          // email textfield
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.deepPurple),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                hintText: "Email",
+                fillColor: Colors.grey[200],
+                filled: true,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+
+          MaterialButton(
+            onPressed: passwordReset,
+            child: Text("Reset Password"),
+            color: Colors.deepPurple[200],
+          ),
+        ],
+      ),
     );
   }
 }
