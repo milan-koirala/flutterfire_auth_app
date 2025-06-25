43a0b439ab77b02aee1b583dd7388ac81c650b11
Milan Koirala <koiralamilon67@gmail.com>
2025-06-25 22:12:36 +0545

    Add Firebase init and notification setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutterfire_auth/pages/auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutterfire_auth/services/noti_service.dart';
 import 'firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Initialize Notification Service
+  NotiService().initNotification();
+
   runApp(const MyApp());
 }
 
@@ -18,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AuthPage()
+      home: AuthPage(),
     );
   }
 }
