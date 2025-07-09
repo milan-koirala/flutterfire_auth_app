2763e940f216e67a6816d1e3c10f008ef48271f0
Milan Koirala <koiralamilon67@gmail.com>
2025-07-09 23:04:48 +0545

    Update AuthPage to redirect logged-in users to DashboardPage instead of HomePage

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterfire_auth/pages/home_page.dart';
+import 'package:flutterfire_auth/pages/dashboard_page.dart';
 import 'package:flutterfire_auth/pages/login_or_register_page.dart';
 
 class AuthPage extends StatelessWidget {
@@ -14,16 +14,14 @@ class AuthPage extends StatelessWidget {
         builder: (context, snapshot) {
           // user is logged in
           if (snapshot.hasData) {
-            return HomePage();
+            return const DashboardPage();
           }
-
           // user is not logged in
           else {
             return LoginOrRegisterPage();
           }
         },
       ),
-
     );
   }
 }
