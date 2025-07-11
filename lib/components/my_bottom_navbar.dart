008bb335f108d5c0bce7457676b466e52f77ec9f
Milan Koirala <koiralamilon67@gmail.com>
2025-07-11 23:18:44 +0545

    Redesigned bottom navigation bar with custom layout and center scan button

--- lib/components/my_bottom_navbar.dart
+++ lib/components/my_bottom_navbar.dart
@@ -12,30 +12,71 @@ class MyBottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: currentIndex,
-      onTap: onTap,
-      selectedItemColor: Colors.red,
-      unselectedItemColor: Colors.grey,
-      type: BottomNavigationBarType.fixed,
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: "Home",
+    return Container(
+      height: 90,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(24),
+          topRight: Radius.circular(24),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.message),
-          label: "Messages",
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            blurRadius: 10,
+            offset: const Offset(0, -1),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _buildNavItem(Icons.home, 0),
+          _buildNavItem(Icons.search, 1),
+          _buildCenterNavItem(2), // center button
+          _buildNavItem(Icons.favorite_border, 3),
+          _buildNavItem(Icons.person_outline, 4),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNavItem(IconData icon, int index) {
+    final isSelected = currentIndex == index;
+    return GestureDetector(
+      onTap: () => onTap(index),
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.red.withOpacity(0.1) : Colors.transparent,
+          borderRadius: BorderRadius.circular(12),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.shopping_cart),
-          label: "Cart",
+        child: Icon(
+          icon,
+          size: 28,
+          color: isSelected ? Colors.red : Colors.grey[400],
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person),
-          label: "Profile",
+      ),
+    );
+  }
+
+  Widget _buildCenterNavItem(int index) {
+    final isSelected = currentIndex == index;
+    return GestureDetector(
+      onTap: () => onTap(index),
+      child: Container(
+        height: 60,
+        width: 60,
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.red : Colors.grey[200],
+          shape: BoxShape.circle,
         ),
-      ],
+        child: Icon(
+          Icons.qr_code_scanner,
+          color: Colors.red,
+          size: 30,
+        ),
+      ),
     );
   }
 }
