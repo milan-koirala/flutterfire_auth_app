466a17aa812eed44569343111acc3ac157634bec
Milan Koirala <koiralamilon67@gmail.com>
2025-07-11 23:32:14 +0545

    Add UserProfile model to represent user data

---
+++ lib/models/user_profile.dart
@@ -0,0 +1,13 @@
+class UserProfile {
+  final String uid;
+  final String name;
+  final String email;
+  final String avatarUrl;
+
+  UserProfile({
+    required this.uid,
+    required this.name,
+    required this.email,
+    required this.avatarUrl,
+  });
+}
